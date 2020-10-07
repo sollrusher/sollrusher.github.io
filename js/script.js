c5ed3c6d08b4a5e340c5702de2021dbb4a376676
Danila <sollrusher@gmail.com>
2020-10-07 22:52:27 +0300

    sukpzdc

--- js/script.js
+++ js/script.js
@@ -44,7 +44,21 @@ textPlace.addEventListener('keypress', function (e) {
 
     textPlace.value = "";
     textPlace.placeholder = "Что вам ещё нужно сделать?";
-    taskReplace();
+    
+    let filter = document.querySelectorAll('.filter');
+    if(filter[0].classList.contains("selected"))
+    {
+         taskReplace();
+    }
+    if(filter[1].classList.contains("selected"))
+    {
+         taskReplaceActive();
+    }
+    if(filter[2].classList.contains("selected"))
+    {
+         taskReplaceCompleted();
+    }
+ 
   }
 
 
@@ -56,7 +70,7 @@ function taskReplace() {
   let returnObj = JSON.parse(localStorage.getItem("todo-list"));
 
   todoList.innerHTML = "";
-
+      let counter = 0;
   for (let raz in returnObj) {
     for (let i = 0; i < returnObj.todos.length; i++) {
 
@@ -67,20 +81,39 @@ function taskReplace() {
       if (returnObj[raz][i].done) {
         done = `active`;
       }
-        todoList.innerHTML += `
-    
+      else{
+        counter ++;
+      }
+         todoList.innerHTML += `
+         <li data-id="${returnObj[raz][i].id}" class="todoItem ${done}">
 
-  <li data-id="${returnObj[raz][i].id}" class="todoItem ${done}">${returnObj[raz][i].title}
-  <div data-id="${returnObj[raz][i].id}" class="delete"></div>
-  </li>
-   `;
+            <input data-id="${returnObj[raz][i].id}" type="checkbox" class='cheks ${done}'>
+            <label>${returnObj[raz][i].title}</label>
+            <button data-id="${returnObj[raz][i].id}" class="delete"></button>
+            
+
+         </li>
+
+         `;
+  
+        
+
 
       }
 
+      let count = document.querySelector('.count');
+      count.innerHTML = `Осталось ${counter}`;
 
       taskDelete();
       doneItem();
     }
+  }    
+  let hideBlock = document.querySelector('.todofooter');
+  if (returnObj.todos.length==0) {
+    hideBlock.style.display='none';
+  }
+  else{
+    hideBlock.style.display='flex';
   }
 }
 taskReplace();
@@ -100,7 +133,19 @@ function taskDelete() {
 
       deleter(i.dataset.id);
 
-      taskReplace();
+      let filter = document.querySelectorAll('.filter');
+      if(filter[0].classList.contains("selected"))
+      {
+           taskReplace();
+      }
+      if(filter[1].classList.contains("selected"))
+      {
+           taskReplaceActive();
+      }
+      if(filter[2].classList.contains("selected"))
+      {
+           taskReplaceCompleted();
+      }
 
     });
   });
@@ -121,7 +166,12 @@ function deleter(idsher) {
 
     }
   }
+  if (returnObj.todos.length==0) {
 
+    let hideBlock = document.querySelector('.todofooter');
+    hideBlock.style.display='none';
+
+  }
 
   let serialObj = JSON.stringify(returnObj); 
   localStorage.setItem("todo-list", serialObj);  
@@ -134,14 +184,14 @@ function deleter(idsher) {
 function doneItem() {
 
 
-  let todoItem = document.querySelectorAll('.todoItem');
+  let cheks = document.querySelectorAll('.cheks');
 
+ 
+  cheks.forEach((i, key) => {
 
-  todoItem.forEach((i, key) => {
+    i.addEventListener('click', (e) => {
 
-    i.addEventListener('click', () => {
-
-
+      
       let returnObj = JSON.parse(localStorage.getItem("todo-list"));
       for (let raz in returnObj) {
         for (let x = 0; x < returnObj.todos.length; x++) {
@@ -164,34 +214,58 @@ function doneItem() {
       }
 
       let serialObj = JSON.stringify(returnObj); 
-      localStorage.setItem("todo-list", serialObj);  
-      taskReplace();
+      localStorage.setItem("todo-list", serialObj); 
+      
+      let filter = document.querySelectorAll('.filter');
+      if(filter[0].classList.contains("selected"))
+      {
+           taskReplace();
+      }
+      if(filter[1].classList.contains("selected"))
+      {
+           taskReplaceActive();
+      }
+      if(filter[2].classList.contains("selected"))
+      {
+           taskReplaceCompleted();
+      }
     });
 
   });
 
 }
 
-let filter = document.querySelectorAll('.footerRight input');
 
+
+let filter = document.querySelectorAll('.filter');
 filter[0].addEventListener('click', (e) => {
 
+  filter[0].classList.add("selected");
+  filter[1].classList.remove("selected");
+  filter[2].classList.remove("selected");
 
   taskReplace();
 
 });
 filter[1].addEventListener('click', (e) => {
 
+  filter[0].classList.remove("selected");
+  filter[1].classList.add("selected");
+  filter[2].classList.remove("selected");
 
   taskReplaceActive();
 
 });
 filter[2].addEventListener('click', (e) => {
-
-
+  
+  filter[0].classList.remove("selected");
+  filter[1].classList.remove("selected");
+  filter[2].classList.add("selected");
+  
   taskReplaceCompleted();
 
 });
+
 
 
 function taskReplaceActive() {
@@ -199,7 +273,7 @@ function taskReplaceActive() {
   let returnObj = JSON.parse(localStorage.getItem("todo-list"));
 
   todoList.innerHTML = "";
-
+  let counter=0;
 
   for (let raz in returnObj) {
     for (let i = 0; i < returnObj.todos.length; i++) {
@@ -209,18 +283,36 @@ function taskReplaceActive() {
 
         todoList.innerHTML += `
     
+        <li data-id="${returnObj[raz][i].id}" class="todoItem">
 
-  <li data-id="${returnObj[raz][i].id}" class="todoItem ">${returnObj[raz][i].title}
-  <div data-id="${returnObj[raz][i].id}" class="delete"></div>
-  </li>
-   `;
+            <input data-id="${returnObj[raz][i].id}" type="checkbox" class='cheks'>
+            <label>${returnObj[raz][i].title}</label>
+            <button data-id="${returnObj[raz][i].id}" class="delete"></button>
+            
 
+         </li>
+
+         `;
+
+
+
+      let count = document.querySelector('.count');
+      counter++;
+      count.innerHTML = `Осталось ${counter}`;
       }
+
 
 
       taskDelete();
       doneItem();
     }
+  }
+  let hideBlock = document.querySelector('.todofooter');
+  if (returnObj.todos.length==0) {
+    hideBlock.style.display='none';
+  }
+  else{
+    hideBlock.style.display='flex';
   }
 }
 
@@ -230,32 +322,46 @@ function taskReplaceCompleted() {
 
   todoList.innerHTML = "";
 
-
+      let counter=0;
 
   for (let raz in returnObj) {
     for (let i = 0; i < returnObj.todos.length; i++) {
 
 
-      let done = '';
-      if (returnObj[raz][i].done) {
-        done = `active`;
-      }
+
 
       if (returnObj && returnObj[raz][i].done == true) {
 
         todoList.innerHTML += `
     
+        <li data-id="${returnObj[raz][i].id}" class="todoItem active">
 
-  <li data-id="${returnObj[raz][i].id}" class="todoItem ${done}">${returnObj[raz][i].title}
-  <div data-id="${returnObj[raz][i].id}" class="delete"></div>
-  </li>
-   `;
+            <input data-id="${returnObj[raz][i].id}" type="checkbox" class='cheks active'>
+            <label>${returnObj[raz][i].title}</label>
+            <button data-id="${returnObj[raz][i].id}" class="delete"></button>
+            
 
+         </li>
+
+         `;
+
+      }
+      else if(returnObj[raz][i].done == true){
+        let count = document.querySelector('.count');
+        counter++;
+        count.innerHTML = `Осталось ${counter}`;
       }
 
       taskDelete();
       doneItem();
     }
+  }
+  let hideBlock = document.querySelector('.todofooter');
+  if (returnObj.todos.length==0) {
+    hideBlock.style.display='none';
+  }
+  else{
+    hideBlock.style.display='flex';
   }
 }
 
@@ -277,18 +383,20 @@ checkAll.addEventListener('click', () => {
       }
       console.log(all);
 
-      if (all < 0) {
-        returnObj[raz][x].done = true;
-        console.log('baga');
-
-      } else {
-        returnObj[raz][x].done = false;
-        console.log('Daga');
-
       }
 
 
     }
+    for (let raz in returnObj) {
+      for (let x = 0; x < returnObj.todos.length; x++) {
+    if (all < 0) {
+      returnObj[raz][x].done = true;
+
+
+    } else {
+      returnObj[raz][x].done = false;
+
+    }}
   }
 
   let serialObj = JSON.stringify(returnObj);
@@ -297,9 +405,9 @@ checkAll.addEventListener('click', () => {
 
 });
 
-let buttons = document.querySelectorAll('.footerLeft input');
+let clearBtn = document.querySelector('.clear');
 
-buttons[0].addEventListener('click', () => {
+clearBtn.addEventListener('click', () => {
 
   let returnObj = JSON.parse(localStorage.getItem("todo-list"));
   for (let raz in returnObj) {
@@ -310,10 +418,18 @@ buttons[0].addEventListener('click', () => {
       }
     }
   }
-  taskReplace();
-});
 
-buttons[1].addEventListener('click', () => {
-  localStorage.clear();
-  taskReplace();
+  let filter = document.querySelectorAll('.filter');
+  if(filter[0].classList.contains("selected"))
+  {
+       taskReplace();
+  }
+  if(filter[1].classList.contains("selected"))
+  {
+       taskReplaceActive();
+  }
+  if(filter[2].classList.contains("selected"))
+  {
+       taskReplaceCompleted();
+  }
 });
